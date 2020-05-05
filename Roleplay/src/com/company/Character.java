3e57df2dd31e7a95c40da06f429817acd311d831
Fitzi99 <64744600+Fitzi99@users.noreply.github.com>
2020-05-05 11:29:55 +0200

    Update Character.java

--- Roleplay/src/com/company/Character.java
+++ Roleplay/src/com/company/Character.java
@@ -1,4 +1,13 @@
 package com.company;
+import java.math.*;
 
 public interface Character {
+	
+	
+	public int performAttack();
+	
+	public double performDefence();
+	
+	public void resetAttack();
+
 }
